504313c7a5ea2ff89976c1c9f4662b5e60d068cb
Samueru-sama <xdglawyer@outlook.com>
2026-04-22 20:26:36 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q planify | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/io.github.alainm23.planify.svg
+export DESKTOP=/usr/share/applications/io.github.alainm23.planify.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/planify \
+	/usr/bin/io.github.alainm23.planify*
 
 # Additional changes can be done in between here
 
